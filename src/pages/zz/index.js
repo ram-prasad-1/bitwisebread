2d532ac4f7104d407a0d726e4a13ce9e4a423a02
ram-prasad-1 <ramprasaddeora@gmail.com>
2022-01-07 21:45:22 +0530

    some changes

---
+++ src/pages/zz/index.js
@@ -0,0 +1,9 @@
+import clsx from 'clsx';
+
+export default function Index() {
+  return (
+    <div className={clsx('container h-64 mx-auto my-12 p-4 max-w-screen-lg bg-green-100')}>
+      Hello world
+    </div>
+  );
+}
